1b3c20e04cfc67a3b47c921d69b1e42d494ddcdd
Li Chongxin <1136142252@qq.com>
2022-03-09 00:37:21 +0800

    week 2 commit

--- src/main/java/com/LiChongxin/week2/demo/HelloWorldServlet.java
+++ src/main/java/com/LiChongxin/week2/demo/HelloWorldServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 /**
  * The type Hello world servlet.
@@ -16,8 +17,12 @@ public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Date datel = new Date();
         PrintWriter writer = resp.getWriter();
         writer.println("Hello Client !!!");
+        writer.println("Name : Li Chongxin");
+        writer.println("Id : 2020211001001128");
+        writer.println("Date and Time :" +datel.toString());
     }
 
     @Override

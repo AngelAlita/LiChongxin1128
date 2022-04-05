d529ecdb03f3ec3d9f2ad74da712b8c9bb042900
Li Chongxin <1136142252@qq.com>
2022-04-05 22:16:35 +0800

    week5 commit

---
+++ src/main/java/com/LiChongxin/week5/demo/LoginServlet.java
@@ -0,0 +1,70 @@
+package com.LiChongxin.week5.demo;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    Connection con = null;
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        ServletContext context = getServletContext();
+        String driver = context.getInitParameter("driver");
+        String url = context.getInitParameter("url");
+        String username = context.getInitParameter("username");
+        String password = context.getInitParameter("password");
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url,username,password);
+            System.out.println("init() - > " + con);
+        }catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String sql = "Select * from usertable where username = ? and password = ?";
+        try {
+            PreparedStatement psmt = con.prepareStatement(sql);
+            psmt.setString(1, username);
+            psmt.setString(2, password);
+            ResultSet rs = psmt.executeQuery();
+            if (rs.next()){
+                // out.print("Login Success!!! <br>");
+                // out.print("Welcome,"+ username +" <br>");
+                request.setAttribute("id",rs.getInt("id"));
+                request.setAttribute("username",rs.getString("username"));
+                request.setAttribute("password",rs.getString("password"));
+                request.setAttribute("email",rs.getString("email"));
+                request.setAttribute("gender",rs.getString("gender"));
+                request.setAttribute("birthDate",rs.getString("birthDate"));
+                request.getRequestDispatcher("userList.jsp").forward(request,response);
+            }else{
+                //  out.print("Login Error!!! <br>");
+                request.setAttribute("message","Username or Password Error!!!");
+                request.getRequestDispatcher("login.jsp").forward(request,response);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

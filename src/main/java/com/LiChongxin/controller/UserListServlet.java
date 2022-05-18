0763c31d9effa4ea18d795c0ec5f04a0d47caf1a
Li Chongxin <1136142252@qq.com>
2022-05-18 22:40:37 +0800

    Week 13 commit

---
+++ src/main/java/com/LiChongxin/controller/UserListServlet.java
@@ -0,0 +1,30 @@
+package com.LiChongxin.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+@WebServlet(name = "UserListServlet", value = "/admin/userList")
+public class UserListServlet extends HttpServlet {
+    Connection con = null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        con = (Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = "/WEB-INF/views/admin/userList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
